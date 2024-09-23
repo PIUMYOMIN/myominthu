4806baa7e64bfc7cc969ea741fae6245af0c4dec
Myo Min Thu <myominthu819@gmail.com>
2024-09-23 21:35:54 +0700

    updatedJ

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://piumyomin.github.io/myominthu",
+  server: {
+    historyApiFallback: true // Ensure SPA routing works in development
+  }
+});
