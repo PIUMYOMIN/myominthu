2df379b82c3913ecad92f7e34264e8d359585039
Myo Min Thu <myominthu819@gmail.com>
2024-10-08 14:29:09 +0700

    updated

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,13 +1,11 @@
-import {
-  createBrowserRouter,
-} from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Home from "../assets/pages/Home";
 import Layout from "../assets/pages/layouts/layout";
 import About from "../assets/pages/About";
 import Service from "../assets/pages/Service";
 import Skills from "../assets/pages/Skills";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,
@@ -17,19 +15,19 @@ const router = createBrowserRouter([
         element: <Home />
       },
       {
-        path: "/about",
+        path: "about", // Remove the leading slash
         element: <About />
       },
       {
-        path: "/services",
+        path: "services",
         element: <Service />
       },
       {
-        path: "/skills",
+        path: "skills",
         element: <Skills />
       }
     ]
-  },
+  }
 ]);
 
 export default router;
